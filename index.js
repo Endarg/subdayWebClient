602431ee3024c66fc2c2d1acc1fa7a95a8885574
Maksim Kuznetsov <m.kuznetsov@kefirgames.com>
2024-01-20 04:28:20 +0300

    add: init

---
+++ index.js
@@ -0,0 +1,319 @@
+import axios from 'https://cdn.jsdelivr.net/npm/axios@1.6.5/+esm'
+
+let isOn = false;
+let channel = "honeymad";
+
+document.body.onload = init;
+
+let gamesRaw;
+let gamesProcessed;
+
+let mainpageRoot;
+let mainpageFormHeader;
+let mainpageStatus;
+
+let mainpageButton;
+let mainpageGamesPanel;
+
+let winpageRoot;
+let winpageFormHeader;
+let winpageWinner;
+let winpageChattersAnouncement;
+let winpageChatters;
+let winpageBackButton;
+let winpageAgainButton;
+
+function init()
+{    
+    buildMainpage();   
+    buildWinnwerpage();
+
+    mountMainpage();
+
+    getInit();
+    getUpdate();
+}
+
+function buildMainpage()
+{
+    mainpageRoot = document.createElement("div");
+    mainpageRoot.className = "mainpage-root";
+
+    const mainpageHeader = document.createElement("div");
+    mainpageHeader.className = "mainpage-header";
+
+    mainpageFormHeader = document.createElement("div");
+    mainpageFormHeader.className = "mainpage-form-header";
+    mainpageFormHeader.textContent = "Subday";
+
+    const mainpageStatusLabel = document.createElement("div");
+    mainpageStatusLabel.className = "mainpage-label"
+    mainpageStatusLabel.textContent = "Статус приема заявок:";
+    
+    mainpageStatus = document.createElement("div");
+    updateStatus();
+
+    mainpageButton = document.createElement("button");
+    mainpageButton.className = "mainpage-button";
+    mainpageButton.textContent = "Выбрать игру";
+    mainpageButton.onclick = mainpageButtonOnClick;
+
+    const mainpageHr01 = document.createElement("hr");
+
+    mainpageGamesPanel = document.createElement("div");
+
+    mainpageRoot.appendChild(mainpageHeader);
+    mainpageHeader.appendChild(mainpageFormHeader);
+    mainpageRoot.appendChild(mainpageStatusLabel);
+    mainpageRoot.appendChild(mainpageStatus);
+    mainpageRoot.appendChild(mainpageButton);
+    mainpageRoot.appendChild(mainpageHr01);
+    mainpageRoot.appendChild(mainpageGamesPanel);
+
+}
+
+function mountMainpage()
+{
+    document.body.appendChild(mainpageRoot);
+}
+
+function dismountMainpage()
+{
+    mainpageRoot.remove();
+}
+
+function buildWinnwerpage()
+{
+    winpageRoot = document.createElement("div");
+    winpageRoot.className = "mainpage-root";
+
+    const winpageHeader = document.createElement("div");
+    winpageHeader.className = "mainpage-header";
+
+    winpageFormHeader = document.createElement("div");
+    winpageFormHeader.className = "mainpage-form-header";
+    winpageFormHeader.textContent = "Subday";
+
+    const winnerpageLabel01 = document.createElement("div");
+    winnerpageLabel01.className = "winpage-label01";
+    winnerpageLabel01.textContent = "Победитель розыгрыша";
+
+    winpageWinner = document.createElement("div");
+    winpageWinner.className = "winpage-winner";
+    winpageWinner.textContent = "Победитель";
+
+    winpageChattersAnouncement = document.createElement("div");
+    winpageChattersAnouncement.className = "winpage-label01";
+    winpageChattersAnouncement.textContent = "варик предложили";
+
+    winpageChatters = document.createElement("div");
+    winpageChatters.className = "winpage-label02";
+    winpageChatters.textContent = "honeymad, endargemp, zhmil";
+
+    const winpageButtonsPanel = document.createElement("div");
+    winpageButtonsPanel.className = "wingape-buttons-panel";
+
+    winpageBackButton = document.createElement("button");
+    winpageBackButton.className = "winpage-button-left";
+    winpageBackButton.textContent = "Назад";
+    winpageBackButton.onclick = wingapeBackButtonOnClick;
+
+    winpageAgainButton = document.createElement("button");
+    winpageAgainButton.className = "winpage-button-right";
+    winpageAgainButton.textContent = "Еще раз";
+    winpageAgainButton.onclick = winpageAgainButtonOnClick;
+
+    winpageRoot.appendChild(winpageHeader);
+    winpageHeader.appendChild(winpageFormHeader);
+    winpageRoot.appendChild(winnerpageLabel01);
+    winpageRoot.appendChild(winpageWinner);
+    winpageRoot.appendChild(winpageChattersAnouncement);
+    winpageRoot.appendChild(winpageChatters);
+    winpageRoot.appendChild(winpageButtonsPanel);
+    winpageButtonsPanel.appendChild(winpageBackButton);
+    winpageButtonsPanel.appendChild(winpageAgainButton);
+}
+
+function mountWinnerpage()
+{
+    document.body.appendChild(winpageRoot);
+}
+
+function dismountWinnerpage()
+{
+    winpageRoot.remove();
+}
+
+function updateStatus()
+{
+    if (isOn)
+    {
+        mainpageStatus.textContent = "Заявки принимаются";
+        mainpageStatus.className = "mainpage-status-on";
+    }
+    else
+    {
+        mainpageStatus.textContent = "Заявки не принимаются";
+        mainpageStatus.className = "mainpage-status-off";
+    }
+}
+
+function updateGamesPanel()
+{
+    removeAllChildNodes(mainpageGamesPanel);
+    for (let i = 0; i < gamesProcessed.length; ++i)
+    {
+        let game = document.createElement("div");
+        let gameName = document.createElement("div");
+        gameName.className = "mainpage-label";
+        gameName.textContent = gamesProcessed[i].name;
+        let gameChance = document.createElement("div");
+        gameChance.className = "mainpage-label-right";
+        gameChance.textContent = gamesProcessed[i].chance+'%';
+        game.appendChild(gameName);
+        game.appendChild(gameChance);
+        mainpageGamesPanel.appendChild(game);
+    }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function chooseWinner()
+{
+    let winnerID = Math.floor(Math.random() * (gamesRaw.length));
+    let winner = gamesRaw[winnerID];
+    winpageWinner.textContent = winner.name;
+
+    let chatters = winner.chatter;
+    let id = 0;
+    for (let i = 0; i < gamesRaw.length; ++i)
+    {
+        if (gamesRaw[i].name == winner.name)
+        {
+            if (gamesRaw[i].chatter != winner.chatter)
+            chatters += ", " + gamesRaw[i].chatter;
+            id++;
+        }
+    }
+
+    if (id > 1)
+    {
+        winpageChattersAnouncement.textContent = "варик предложили";
+    }
+    else
+    {
+        winpageChattersAnouncement.textContent = "варик предложил";
+    }
+
+    winpageChatters.textContent = chatters;
+}
+
+function mainpageButtonOnClick()
+{
+    chooseWinner();
+    dismountMainpage();
+    mountWinnerpage();
+}
+
+function wingapeBackButtonOnClick()
+{
+    dismountWinnerpage();
+    mountMainpage();
+}
+
+function winpageAgainButtonOnClick()
+{
+    chooseWinner();
+}
+
+function processGames(games)
+{
+    gamesProcessed = [];
+    
+    for (let i = 0; i < gamesRaw.length; ++i)
+    {
+        let alreadyProcessed = false;
+        for (let j = 0; j < gamesProcessed.length; ++j)
+        {
+            if (gamesRaw[i].name == gamesProcessed[j].name)
+            {
+                alreadyProcessed = true;
+                gamesProcessed[j].votes++;
+                break;
+            }
+        }
+
+        if (!alreadyProcessed)
+        {
+            gamesProcessed.push({
+                name: gamesRaw[i].name,
+                votes: 1,
+                chance: 0
+            });
+        }
+    }
+
+    for (let i = 0; i < gamesProcessed.length; ++i)
+    {
+        gamesProcessed[i].chance = (gamesProcessed[i].votes * 100 / (gamesRaw.length)).toString().slice(0,4);
+    }
+
+    gamesProcessed = quickSort(gamesProcessed);
+}
+
+function quickSort(arr) {
+    
+    if (arr.length < 2) return arr;
+
+    let pivot = arr[0].votes;
+    let left = [];
+    let right = [];
+      
+    for (let i = 1; i < arr.length; i++) {
+      if (pivot < arr[i].votes) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
+      }
+    };
+
+    return quickSort(left).concat(arr[0], quickSort(right));
+}
+
+async function getInit()
+{
+    await axios.get('http://localhost:5000/get-init').then (res =>{
+        gamesRaw = res.data.games;
+        isOn = res.data.isOn;
+        processGames();
+        updateGamesPanel();
+        updateStatus();
+    });
+    
+}
+
+async function getUpdate()
+{
+
+    try {
+        console.log('Getting update');
+        
+        await axios.get('http://localhost:5000/get-update').then (res => {
+            gamesRaw = res.data.games;
+            isOn = res.data.isOn;
+            processGames();
+            updateGamesPanel();
+            updateStatus();
+        });
+        
+        await getUpdate()
+    } catch (e) {
+        setTimeout ( () => {
+            getUpdate()
+        }, 500)
+    }
+}
